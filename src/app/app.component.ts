36a7eee966e9e5a3525d1a5f5f7d7b41833c83ef
evandrogouveia <vando_gouveia@hotmail.com>
2020-05-23 12:36:07 -0300

    add styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { ExcelService } from './services/excel.service';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'exportar-excel-demo';
+export class AppComponent implements OnInit {
+  data: any = [];
+  @ViewChild('table') table: ElementRef;
+
+  constructor(private excelService: ExcelService) {
+  }
+
+  ngOnInit() {
+    this.estados();
+  }
+
+  estados() {
+    this.excelService.getEstados().subscribe(uf => {
+      this.data = uf;
+    });
+  }
+
+  exportAsExcelFile(nomeArquivo, excelFileName: string): void {
+    const worksheet: XLSX.WorkSheet = XLSX.utils.table_to_sheet(this.table.nativeElement);
+
+    const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet}, SheetNames: ['data'] };
+
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+
+    this.excelService.saveAsExcelFile(excelBuffer, excelFileName);
+  }
+
+  exportAsXLSX(): void {
+    this.exportAsExcelFile(this.data, 'estados');
+  }
+
 }
---
+++ src/app/services/excel.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import * as FileSaver from 'file-saver';
+import { HttpClient } from '@angular/common/http';
+
+const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+
+const EXCEL_EXTENSION = '.xlsx';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExcelService {
+
+  readonly apiUrl: string = 'https://servicodados.ibge.gov.br/api/v1/localidades';
+
+  constructor(private http: HttpClient) { }
+
+  getEstados() {
+    return this.http.get(`${this.apiUrl}/estados`);
+  }
+
+  public saveAsExcelFile(buffer: any, fileName: string): void {
+    const data: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE
+    });
+
+    FileSaver(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+  }
+}
